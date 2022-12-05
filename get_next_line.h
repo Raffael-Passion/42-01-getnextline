4fd05b00e76a8dbfa69af22243d72cc7402b07b2
Raffael Hortensteiner <rhortens@2-E-3.42heilbronn.de>
2022-12-05 21:11:57 +0100

    getnextline

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rhortens <rhortens@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/05 20:18:20 by rhortens          #+#    #+#             */
+/*   Updated: 2022/12/05 21:00:35 by rhortens         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <stdio.h>
+
+char	*get_next_line(int fd);
+
+#endif
